5d22f11584a23b52f86749e85630a458732969b3
Tom Boyd <tom@boyd.io>
2025-07-29 03:32:09 -0700

    f

--- keyboards/planck/rev7/keymaps/tombplnck/user_song_list.h
+++ keyboards/planck/rev7/keymaps/tombplnck/user_song_list.h
@@ -1,3 +1,3 @@
 #include "musical_notes.h"
 
-#define CP2077_V S__NOTE(_A2), S__NOTE(_A2), S__NOTE(_A2), S__NOTE(_REST), S__NOTE(_REST), S__NOTE(_A2), S__NOTE(_REST), S__NOTE(_REST), S__NOTE(_GS3), S__NOTE(_GS3), S__NOTE(_GS3), S__NOTE(_GS3), S__NOTE(_A3), S__NOTE(_A3), S__NOTE(_A3), S__NOTE(_A3), S__NOTE(_A2), S__NOTE(_A2), S__NOTE(_A2), S__NOTE(_REST), S__NOTE(_REST), S__NOTE(_A2), S__NOTE(_REST), S__NOTE(_GS3), S__NOTE(_GS3), S__NOTE(_GS3), S__NOTE(_GS3), S__NOTE(_A3), S__NOTE(_A3), S__NOTE(_A3), S__NOTE(_A3),
+#define CP2077_V S__NOTE(_A2), S__NOTE(_A2), S__NOTE(_A2), S__NOTE(_REST), S__NOTE(_REST), S__NOTE(_A2), S__NOTE(_REST), S__NOTE(_REST), S__NOTE(_GS3), S__NOTE(_GS3), S__NOTE(_GS3), S__NOTE(_GS3), S__NOTE(_A3), S__NOTE(_A3), S__NOTE(_A3), S__NOTE(_A3), S__NOTE(_A2), S__NOTE(_A2), S__NOTE(_A2), S__NOTE(_REST), S__NOTE(_REST), S__NOTE(_A2), S__NOTE(_REST), S__NOTE(_GS3), S__NOTE(_GS3), S__NOTE(_GS3), S__NOTE(_GS3), S__NOTE(_A3), S__NOTE(_A3), S__NOTE(_A3), S__NOTE(_A3)
