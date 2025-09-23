1ea7c06904ce283334b529f8ff5fdc879455a90a
Tom Boyd <tom@boyd.io>
2025-09-22 20:12:55 -0700

    f

--- keyboards/planck/rev7/keymaps/tombplnck/keymap.c
+++ keyboards/planck/rev7/keymaps/tombplnck/keymap.c
@@ -33,21 +33,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_planck_2x2u(
           QK_GESC,  KC_Q,         KC_W,     KC_E,     KC_R,            KC_T,  KC_Y,    KC_U,     KC_I,     KC_O,          KC_P,     KC_BSPC,
           KC_LCTL,  KC_A,         KC_S,     KC_D,     KC_F,            KC_G,  KC_H,    KC_J,     KC_K,     KC_L,          KC_SCLN,  KC_QUOT,
-          KC_LSFT,   KC_Z,         KC_X,     KC_C,     KC_V,            KC_B,  KC_N,    KC_M,     KC_COMM,  KC_DOT,        KC_SLSH, RSFT_T(KC_ENT) ,
+          KC_LSFT,   KC_Z,         KC_X,     KC_C,     KC_V,            KC_B,  KC_N,    KC_M,     KC_COMM,  KC_DOT,        KC_SLSH, KC_RSFT ,
           NAV,    LCA(KC_DEL),  KC_LGUI,  KC_LALT,  KC_TAB,                          KC_SPC,  KC_LEFT,  KC_DOWN,  KC_UP,    LT(_SYM,KC_RGHT)
     ),
       /* MO1 Nav, Numbers */
     [_NAV] = LAYOUT_planck_2x2u(
           KC_TRNS,  KC_1,         KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL,
           KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_TRNS,  KC_TRNS,
-          KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   KC_BSLS,  KC_TRNS,
-          NAV,  LCA(KC_DEL),  KC_LGUI,  KC_LALT,  KC_LSFT,                              KC_ENT,   KC_TRNS,  KC_TRNS,  KC_TRNS,   SYM
+          KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   KC_BSLS,  KC_ENT,
+          NAV,  LCA(KC_DEL),  KC_LGUI,  KC_LALT,  KC_LSFT,                              KC_RSFT,   KC_TRNS,  KC_TRNS,  KC_TRNS,   SYM
     ),
       /* MO2 Symbols, Numbers */
     [_SYM] = LAYOUT_planck_2x2u(
           KC_TRNS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_TRNS,
           KC_TRNS,  KC_LCBR,  KC_RCBR,  KC_UNDS,  KC_PIPE,  KC_LT,    KC_GT,    KC_SLSH,  KC_MINS,  KC_LBRC,  KC_RBRC,  KC_TRNS,
-          KC_TRNS,  KC_TRNS,  KC_DOT,   KC_PPLS,  KC_LPRN,  KC_SCLN,  KC_COLN,  KC_RPRN,  KC_PMNS,  KC_EQL,  KC_TRNS,  KC_TRNS,
+          KC_ENT,  KC_TRNS,  KC_DOT,   KC_PPLS,  KC_LPRN,  KC_SCLN,  KC_COLN,  KC_RPRN,  KC_PMNS,  KC_EQL,  KC_TRNS,  KC_TRNS,
           NAV,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LSFT,  KC_ENT,   KC_TRNS,  KC_TRNS,  KC_TRNS,   SYM
     ),
       /* Settings */
@@ -59,10 +59,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      ),
       /* CyberPunk 2077 */
     [_CP77] = LAYOUT_planck_2x2u(
-          KC_ESC,  QK_GESC,  KC_TAB, KC_1,     KC_2,  KC_3,    KC_4,  KC_5,   KC_6,     KC_NO,  KC_NO,    BASE,
-          KC_NO,   KC_G,     KC_B,     KC_Q,  KC_W,    KC_E,  KC_R,   KC_T,    KC_Y,   KC_U,   KC_I,   KC_O,
-          KC_NO,   KC_LSFT,  KC_Z,  KC_A,  KC_S,    KC_D,  KC_F,   KC_NO,  KC_NO,  KC_NO, KC_TRNS, KC_TRNS,
-          NAV,     KC_LCTL,  KC_X,  KC_Y,  KC_SPC,                           KC_C,  KC_P,  KC_NO,  KC_NO, SYM
+          KC_ESC,  QK_GESC, KC_1,  KC_2,     KC_TAB, KC_3,  KC_4,  KC_5,    KC_4,   KC_5,   KC_6,      BASE,
+          KC_M,    KC_P,    KC_G,  KC_B,     KC_Q,   KC_W,  KC_E,  KC_R,    KC_NO,  KC_NO,  KC_NO,     KC_NO,
+          KC_O,    KC_K,    KC_T,  KC_RSFT,  KC_A,   KC_S,  KC_D,  KC_F,    KC_NO,  KC_NO,  KC_TRNS, KC_NO,
+          NAV,     KC_LCTL, KC_X,  KC_LCTL,  KC_Y,                         KC_SPC,  KC_NO,  KC_NO,  KC_NO,      SYM
      ),
     [_CS2] = LAYOUT_planck_2x2u(
       /* CS2 */
